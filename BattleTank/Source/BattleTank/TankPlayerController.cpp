7cb4adb59031a25fa1dd6d3017f7e917b8048cdd
Chrismar <chrismar303@gmail.com>
2017-07-05 22:33:04 -0700

    Created TankPlayerController and Tank class &&& Tank_BP and TankPlayerController_BP reparented to Tank.cpp & TankPlayerController.cpp.

---
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankPlayerController.h"
+#include "GameFramework/Pawn.h"
+
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ATank* tank = GetControllerTank();
+	if (tank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("SUPER::BEGINPLAY --- %s"), *tank->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("SUPER::BEGINPLAY --- NO POSSESSED TANK"));
+	}
+
+}
+
+
+ATank* ATankPlayerController::GetControllerTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+
